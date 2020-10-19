b7cbbba69c7f91cfe3ddec4bb1f92c4f8d290195
timislearning <tim.mitzen@gmail.com>
2020-10-19 13:02:59 -0400

    Cleaned up some code

--- basketball.py
+++ basketball.py
@@ -13,20 +13,11 @@ user_score = []
 cpu_score = []
 
 
-def cpu_two_pointer(two):
-    two_cpu_result = two
-    if two == 2:
-        if random.random() <= .10:
-            cpu_score.append(two_cpu_result)
-            return print("Cpu was fouled and made two free throws")
-        elif random.random() <= .45:
-            cpu_score.append(two_cpu_result)
-            return print('Other team scored 2 points')
-        else:
-            return print('Other team missed')
 
-def two_pointer(two):
+
+def two_pointer(two, cpu_two):
     two_result = two
+    cpu_result = cpu_two
     if two == 2:
         if random.random() <= .10:
             user_score.append(two_result)
@@ -34,11 +25,22 @@ def two_pointer(two):
         elif random.random() <= .45:
             user_score.append(two_result)
             return print('You scored two points')
-        else:
+        if random.random() <= .10:
+            cpu_score.append(cpu_result)
+            return print("Cpu was fouled and made two free throws")
+        elif random.random() <= .45:
+            cpu_score.append(cpu_result)
+            return print('Other team scored 2 points')
+        elif random.random() <=.70:
+            return print('Other team missed')
+        elif random.random() <=.70:
             return print('You missed')
+        else:
+            print("Ball out of bounds")
 
-def three_pointer(three):
+def three_pointer(three, cpu_three):
     three_result = three
+    cpu_three_result = cpu_three
     if three == 3:
         if random.random() <= .05:
             user_score.append(three_result)
@@ -46,21 +48,21 @@ def three_pointer(three):
         elif random.random() <= .3:
             user_score.append(three_result)
             return print('you scored a three pointer')
-        else:
-            return print('You missed')
 
-
-def cpu_three_pointer(three):
-    cpu_three_result = three
-    if three == 3:
-        if random.random() <= .05:
+        elif random.random() <= .05:
             cpu_score.append(cpu_three_result)
             return print('Cpu was fouled and made 3 free throws')
         elif random.random() <= .30:
             cpu_score.append(cpu_three_result)
             print('Cpu scored a three')
+        elif random.random() <= .70:
+            return print('You missed')
+        elif random.random() <= .70:
+            print("Cpu missed")
         else:
-            return print('Cpu missed')
+            return print("Ball out of bounds")
+
+
 
 
 end_time = datetime.now() + timedelta(seconds = 45)# gives 45 seconds to play
@@ -74,16 +76,16 @@ while datetime.now() < end_time:
         print(f'Your score is {player_sum} and cpu score is {cpu_sum} game was not finished')
         break
     if user_input == '2':
-        two_pointer(2)
-        cpu_two_pointer(2)
+        two_pointer(2,2)
+
         player_sum = sum(user_score)
         cpu_sum = sum(cpu_score)
 
         print(f'Player score is: {player_sum}\nCpu score is: {cpu_sum}')
 
     elif user_input == '3':
-        three_pointer(3)
-        cpu_three_pointer(3)
+        three_pointer(3,3)
+
         player_sum = sum(user_score)
         cpu_sum = sum(cpu_score)
         print(f'User score is: {player_sum}\nCpu score is: {cpu_sum}')
